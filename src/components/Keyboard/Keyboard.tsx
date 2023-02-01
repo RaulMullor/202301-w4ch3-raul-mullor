2af965335ea22a2fb2c1c2ad42b2def881f64307
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-01 22:00:34 +0100

    Add function to add numbers to Keyboard Component

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,21 +1,14 @@
 import { Key } from "../Key/Key";
+import { numbers } from "../../data/numbers";
 import "./Keyboard.css";
 
 export const Keyboard = (): JSX.Element => {
   return (
     <div className="keyboard-container">
       <ol className="keyboard">
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
-        <Key />
+        {numbers.map((number) => (
+          <Key keyNumber={number} />
+        ))}
       </ol>
     </div>
   );
