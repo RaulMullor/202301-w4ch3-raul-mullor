fb4387427e2147ca6f70be3cd5e4bc4271232aac
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-02 21:21:59 +0100

    Create functionality for Action call button

--- src/components/PhoneContextProvider/PhoneContextProvider.tsx
+++ src/components/PhoneContextProvider/PhoneContextProvider.tsx
@@ -9,13 +9,20 @@ const PhoneContextProvider = ({
   children,
 }: PhoneContextProviderProps): JSX.Element => {
   const [phoneNumber, setPhoneNumber] = useState("");
+  let [callState, setCallState] = useState(false);
 
   const addNumbers = (number: string): void => {
     setPhoneNumber(phoneNumber + number);
   };
 
+  const isCalling = (): void => {
+    setCallState((callState = true));
+  };
+
   return (
-    <PhoneContext.Provider value={{ addNumbers, phoneNumber }}>
+    <PhoneContext.Provider
+      value={{ addNumbers, phoneNumber, isCalling, callState }}
+    >
       {children}
     </PhoneContext.Provider>
   );
--- src/store/context/PhoneContext.ts
+++ src/store/context/PhoneContext.ts
@@ -3,6 +3,8 @@ import { createContext } from "react";
 interface PhoneContextStructure {
   phoneNumber: string;
   addNumbers: (number: string) => void;
+  callState: boolean;
+  isCalling: () => void;
 }
 
 export const PhoneContext = createContext({} as PhoneContextStructure);
